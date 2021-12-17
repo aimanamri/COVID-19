dc56c926e22bdcad073b8f57b237999b11fc232f
Aiman Amri <aimanuddin@gmail.com>
2021-12-17 09:17:45 +0900

    Automatically updated on 17/12/ Fri at  9:17

--- push_to_git.bat
+++ push_to_git.bat
@@ -1,6 +1,6 @@
 cd B:\COVID-19-Analysis\
 git pull
 git add .
-git commit -m "Auto commit on %date:~0,2%/%date:~3,2%/%date:~-4% at %time:~0,2%:%time:~3,2%"
+git commit -m "Automatically updated on %date:~0,2%/%date:~3,2%/%date:~-4% at %time:~0,2%:%time:~3,2%"
 git push
 exit
